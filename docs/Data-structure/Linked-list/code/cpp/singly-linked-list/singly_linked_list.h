4b21bd6efc554452430e7d2fa0d94ed28216ba88
dengking <2238897886@qq.com>
2022-12-05 23:02:20 +0800

    更新内容

--- docs/Data-structure/Linked-list/code/cpp/singly-linked-list/singly_linked_list.h
+++ docs/Data-structure/Linked-list/code/cpp/singly-linked-list/singly_linked_list.h
@@ -20,23 +20,28 @@ public:
     SinglyLinkedList(const SinglyLinkedList &other) = delete;
     SinglyLinkedList &operator=(const SinglyLinkedList &other) = delete;
 
-    /// @brief
+    /// @brief 
+    /// 一、append to tail
+    /// 二、需要考虑队列是否为空
     /// @param new_value
     void push(T new_value)
     {
         std::unique_ptr<Node> new_node(new Node(std::move(new_value)));
         Node *const new_tail = new_node.get();
-        if (tail_)
+        if (tail_) // 队列非空
         {
             tail_->next_ = std::move(new_node);
         }
-        else
+        else // 队列为空
         {
             head_ = std::move(new_node);
         }
         tail_ = new_tail; // 总是让tail指向new tail_
     }
-    /// @brief 以迭代的方式获得链表节点的个数
+    
+    /// @brief 以迭代的方式获得链表节点的个数，由于本实现中是使用的smart pointer std::unique_ptr来管理node的，
+    /// 因此本方法的实现需要对smart pointer的使用有一定的了解: 由于我们是iteration，仅仅access node而已，因此使用
+    /// `std::unique_ptr.get()`
     /// @return
     size_t get_size_by_iteration()
     {
